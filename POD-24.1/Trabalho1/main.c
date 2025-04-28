a6d27744610d04a5ca2d97d7f25ec011ea4d11a7
TaiBarzotto <taibernardi99@gmail.com>
2025-04-27 23:34:20 -0300

    Feat: Feita a parte de leitura e ordenação

--- POD-24.1/Trabalho1/main.c
+++ POD-24.1/Trabalho1/main.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ordem.h"
 #define MEMORIA_MAX 1000000
 
 int main() {
+    
     int M;
+    char c;
     printf("Digite a quantidade de elementos a serem carregados por vez em memória (M): ");
     scanf("%d", &M);
-
+    
+    int aux_arr[M];
     if (M <= 0 || M > MEMORIA_MAX) {
         printf("Valor de M inválido. Deve ser entre 1 e %d.\n", MEMORIA_MAX);
         return 0;
+    }
+    FILE *entrada = fopen("dados.txt", "r");
+    if (entrada == NULL) {
+        printf("Erro ao abrir o arquivo de entrada.\n");
+        return 0;
+    }
+
+    while(!feof(entrada)) {
+        int i = 0;
+        while (i < M) {
+            c = fgetc(entrada);
+            if (c == ';'){
+                continue;
+            }
+            aux_arr[i] = (int) c;
+            i++;
+        }
+        quickSort(aux_arr, 0, M-1);
+        // até aqui to lendo e ordenando, falta fazer a parte dos arquivos
+        printf("Lidos %d elementos:\n", M);
+        for (int i = 0; i <= M; i++) {
+            printf("%c ", aux_arr[i]);
+        }
+        printf("\n");
     }
 
     //ordenacao_externa(M);
--- POD-24.1/Trabalho1/ordem.h
+++ POD-24.1/Trabalho1/ordem.h
@@ -3,5 +3,4 @@
 void troca(int *a, int *b);
 int particiona(int arr[], int inicio, int fim);
 void quickSort(int arr[], int inicio, int fim);
-void ordenacao_externa(int M);
 #endif
