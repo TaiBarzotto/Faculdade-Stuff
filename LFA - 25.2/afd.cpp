339195fc22cba1eaaac903940ac4e59adc8a1857
Tai Barzotto <taibernardi99@gmail.com>
2025-10-26 17:51:41 -0300

    Feat: codigo inicial para a geração de uma AFND em afd.cpp

--- LFA - 25.2/afd.cpp
+++ LFA - 25.2/afd.cpp
@@ -13,27 +13,82 @@ int main(){
     }
 
     set<char> simbolos;
+    int count_simbolos =0;
     char c;
-    vector<char> estados;
     while (!feof(tokens))
     {
         c = fgetc(tokens);
         if (c!=' ' && c!=EOF && c!= '\n')
         {
-            
             simbolos.insert(c);
+            count_simbolos++;
         }
         
     }
-    pair<char, int> dict_simbolos;
-
+    
+    unordered_map<char,int> dict_simbolos;
     int i = 0;
     for (char s : simbolos) {
-        cout << s <<"-"<<i<< " ";
-        dict_simbolos.first = s;
-        dict_simbolos.second = i;
+        dict_simbolos[s] = i;
         i++;
     }
+    
+    vector<vector<string>> afnd(count_simbolos, vector<string>(i, ""));
+    rewind(tokens);
+    int prox_estado_livre = 0;
+    int eh_simbolo_inicial = 1;
+    int estado_inicial = 0;
+    int estado_atual = 0;
+    while ((c = fgetc(tokens)) != EOF)
+    {
+        if (c != ' ')
+        {
+            if (c!='\n')
+            { 
+                if (eh_simbolo_inicial)
+                {
+                    estado_atual = estado_inicial;
+                }else
+                {
+                    estado_atual = prox_estado_livre;
+                }
+                
+                
+                auto it = dict_simbolos.find(c);
+                if (it != dict_simbolos.end()) {
+                    int idx = it->second;
+                    afnd[estado_atual][idx] += (afnd[estado_atual][idx].empty() ? to_string(++prox_estado_livre) : ", " + to_string(++prox_estado_livre));
+                }
+                
+                eh_simbolo_inicial=0;
+            }else
+            {
+                eh_simbolo_inicial = 1;
+            }
+            
+        }
+    }
+
+    cout<<"\t";
+    for (char s : simbolos) {
+        cout<<s<<"\t";
+    }
+    cout<<endl;
+
+    for (int r = 0; r < afnd.size(); ++r) {
+        cout << r << "\t";
+        for (int col = 0; col < afnd[r].size(); col++) {
+            if (afnd[r][col] == "")
+                cout << "-\t";
+            else
+                cout << afnd[r][col] << "\t";
+        }
+        cout << '\n';
+    }
+
+
+    
+    
 
     fclose(tokens);
     return 0;
